2671bfb6237de5216b7a7575a25199dd8029f3db
Joel Alderete <thej14653@gmail.com>
2024-09-05 11:18:20 -0600

    Add files via upload

---
+++ EVA_10_OPERACIONES2/src/eva_10_operaciones2/EVA_10_OPERACIONES2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_10_operaciones2;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA_10_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Alderete Aguirre Diego Joel
+        
+        //Concatenacion en Cadena
+        
+        String Nombre = "Joel";
+        String Apellido = "Alderete";
+        String NC;
+        
+        NC = Nombre + " " + Apellido;
+        System.out.println("Su nombre es: " + Nombre);
+        System.out.println("Su apellido es; " + Apellido);
+        System.out.println("Nombre y Apellido: " + NC);
+        
+        
+    }
+    
+}
